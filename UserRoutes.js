fced02d5c7d58cc5ea711c0bba1cea17fc8575bc
Bunty <kr.suraj1602@gmail.com>
2023-07-11 08:25:53 +0530

    final

---
+++ UserRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const registerUser = require('./UserController');
+
+router.route('/register').post(registerUser);
+router.route('/login').get(registerUser);
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const router = require('./UserRoutes');
+
+const app = express();
+const port = 4000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/api/v1', router);
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/api/v1/register', (req, res) => {
+	const userName = req.body.name;
+	const userEmail = req.body.email;
+	const userPassword = req.body.password;
+	res.json({
+		success: true,
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Server is working on port: ${port}`);
+});
